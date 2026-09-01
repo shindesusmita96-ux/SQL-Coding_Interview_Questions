83626eb05ce67db8f2f6772024caea0be2bed359
Susmita <shindesusmita96@gmail.com>
2026-09-01 17:51:26 +0530

    Uncomment SQL query for final account balance

---
+++ question_4.sql
@@ -0,0 +1,9 @@
+-- Question Link: https://datalemur.com/questions/final-account-balance
+
+SELECT account_id,
+SUM(CASE WHEN
+      transaction_type = 'Deposit' THEN amount
+      ELSE -amount
+      END) AS final_balance
+FROM transactions
+GROUP BY account_id;
