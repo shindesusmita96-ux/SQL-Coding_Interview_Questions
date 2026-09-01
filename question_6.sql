0a923600690bb81a31d761f02b93cb3b0814798d
Susmita <shindesusmita96@gmail.com>
2026-09-01 18:31:27 +0530

    Add SQL query for card issued amount difference
    Add SQL query to calculate the difference in issued amounts for each card.

---
+++ question_6.sql
@@ -0,0 +1,9 @@
+-- Question Link: https://datalemur.com/questions/cards-issued-difference
+
+-- Solution:
+
+SELECT card_name,
+MAX(issued_amount)-MIN(issued_amount) AS difference
+FROM monthly_cards_issued
+GROUP BY card_name
+ORDER BY difference DESC;
